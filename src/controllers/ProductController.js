2066e312f1ec1bda03babdcc158386621f552a19
Lucas <lucaslops12@gmail.com>
2024-02-05 15:24:18 -0300

    add product to the system

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,7 +1,47 @@
-import Product from '../models/Products.js'
+import Product from "../models/Products.js";
 
 export default class UserController {
-    static async createProduct(req, res){
-        const {name, description, price, quantity} = req.body
+  static async createProduct(req, res) {
+    const { name, description, price, quantity } = req.body;
+
+    const images = req.file;
+
+    switch (true) {
+      case !name:
+        res.status(422).json({ message: "O nome é obrigatório!" });
+        return;
+      case !description:
+        res.status(422).json({ message: "A descrição é obrigatória!" });
+        return;
+      case !price:
+        res.status(422).json({ message: "O preço é obrigatório!" });
+        return;
+      case !quantity:
+        res.status(422).json({ message: "A quantidade é obrigatória!" });
+        return;
+      case !images:
+        res.status(422).json({ message: "A imagem é obrigatória!" });
+        return;
+      case quantity < 1:
+        res.status(422).json({ message: "A quantidade é obrigatória!" });
+        return;
+      default:
+        break;
     }
+
+    try {
+      const newProduc = await Product.create({
+        name,
+        description,
+        price,
+        quantity,
+        images: images.filename,
+      });
+      res.status(200).json({ message: "Produto criado com sucesso!" });
+    } catch (error) {
+      console.error("Erro durante o registro de produto:", error);
+      res.status(500).json({ message: "Erro durante o registro do produto." });
+      return;
+    }
+  }
 }
--- src/routes/ProductsRoutes.js
+++ src/routes/ProductsRoutes.js
@@ -3,8 +3,15 @@ import express from "express";
 import ProductController from "../controllers/ProductController.js";
 import checkAdmin from "../utils/CheckAdmin.js";
 
+import { imageUpload } from "../utils/ImageUpload.js";
+
 const router = express.Router();
 
-router.post("/create", checkAdmin, ProductController.createProduct);
+router.post(
+  "/create",
+  checkAdmin,
+  imageUpload.single("images"),
+  ProductController.createProduct
+);
 
 export default router;
