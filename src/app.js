90de3d1fa4d3649d452ce17b68f2e0410b70cdb7
Lucas <lucaslops12@gmail.com>
2024-02-17 16:13:23 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import ProductsRoutes from './routes/ProductsRoutes.js'
 import UserRoutes from './routes/UserRoutes.js'
 import sequelize from './database/conn.js';
 import Associations from './models/Associations.js';
+
+import cors from 'cors'
 Associations();
 
 //sequelize.sync()
@@ -12,7 +14,8 @@ Associations();
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
+app.use(express.static("public"));
 
 app.use("/users", UserRoutes)
 app.use("/products", ProductsRoutes)
